01f613bc1c04776bd351297448fa95b5a84208df
Andrea Graziani <andrea.graziani93@outlook.it>
2019-06-13 14:30:04 +0200

    Update

---
+++ src/test/java/TestCuttingStockInstance.java
@@ -0,0 +1,13 @@
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestCuttingStockInstance {
+
+    @Test
+    public void test() {
+        assertEquals(4.0, 4.0);
+    }
+}
